125c459d30bca4f038f81089c7b8265c5c2606bb
Jugpreet Singh <jugpreet89@gmail.com>
2019-01-05 00:28:44 -0800

    countdown app initial commit with CRA

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EventList from './EventList.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <EventList/>
+    );
+  }
+}
---
+++ EventList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+class EventList extends React.Component {
+    render() {
+        return (
+            <Text>Hello! from EventList</Text>
+        )
+    }
+}
+
+export default EventList;
