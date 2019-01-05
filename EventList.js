2478abc9a5a2b6fe6ee02812c216f97a22edba56
Jugpreet Singh <jugpreet89@gmail.com>
2019-01-05 01:51:38 -0800

    counting down event list with static event data

--- EventList.js
+++ EventList.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, FlatList, StyleSheet} from 'react-native';
+import EventCard from './EventCard';
+
+const styles = StyleSheet.create({
+    eventList: {
+        flex: 1,
+        paddingTop: 40,
+        backgroundColor: '#F3F3F3'
+    }
+});
 
 class EventList extends React.Component {
+
+    state = {
+        events: []
+    };
+
+    componentDidMount() {
+
+        setInterval(() => {
+            this.setState({
+                events: this.state.events.map(e => ({
+                    ...e,
+                    timer: Date.now(),
+                })),
+            })
+
+        }, 1000);
+        const events = require('./db.json').events.map(e => ({
+            ...e,
+            date: new Date(e.date)
+        }));
+        this.setState({events: events});
+    };
     render() {
         return (
-            <Text>Hello! from EventList</Text>
+            <FlatList style={styles.eventList}
+                data = {this.state.events}
+                renderItem = {({item}) => <EventCard event={item}/>}
+                keyExtractor = {item => item.id}
+            />
         )
     }
 }
